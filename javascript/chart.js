022982e1f32cfdd3bdb156454b978fdd3f26be9b
Travis Glenn Hansen <travisghansen@yahoo.com>
2011-12-22 17:23:59 -0700

    start of viewport size code

--- javascript/chart.js
+++ javascript/chart.js
@@ -1,6 +1,7 @@
 var page = require('webpage').create();
 var fs = require('fs');
 
+page.viewportSize = { width: 1280, height: 1024 };
 page.content = fs.read (phantom.args[0]);
 
 console.log(page.evaluate(function () {
